deb944feb66b3afa499e6929b274904b76f9188a
calebkumar <kumarcal@grinnell.edu>
2018-05-25 16:34:38 -0700

    Put data in database : )

--- python/transaction_service.py
+++ python/transaction_service.py
@@ -2,21 +2,27 @@ from flask import jsonify
 # from transaction import Transaction
 from datetime import datetime
 from pony.orm import db_session
+import pony.orm as pny
 from database import Transaction
 
 
+@pny.db_session
 class TransactionService:
     def __init__(self):
         print("")
 
+    @pny.db_session
     def storeTransactions(self, transactionsResponse):
-        with db_session:
-            for transactionResponse in transactionsResponse:
-                transactionEntity = Transaction(transaction_id=transactionResponse['transaction_id'],
-                                                name=str(transactionResponse['name']),
-                                                amount=transactionResponse['amount'],
-                                                date=transactionResponse['date'],
-                                                default_transaction_type=transactionResponse['transaction_type'],
-                                                default_transaction_categories=transactionResponse['category'])
+        for transactionResponse in transactionsResponse:
+            print transactionResponse
+
+            transactionEntity = Transaction(id=transactionResponse['transaction_id'],
+                                            account_id=transactionResponse['account_id'],
+                                            # account_owner='Caleb Kumar',
+                                            name=str(transactionResponse['name']),
+                                            amount=transactionResponse['amount'],
+                                            date=datetime.strptime(transactionResponse['date'], '%Y-%m-%d'),
+                                            default_transaction_type=transactionResponse['transaction_type'],
+                                            default_category1=str(transactionResponse['category']))
 
             print(transactionEntity)
